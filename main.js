d4b6499a377b94d8887f7491f47836f623b58d83
Luiz Felipe <luizfelipesmaciel@gmail.com>
2020-01-27 23:45:04 -0300

    Added Player interface to skip musics every 3 secs

--- main.js
+++ main.js
@@ -7,11 +7,26 @@ let bus = DBus.getBus('session');
 let spotify = {
     service: 'org.mpris.MediaPlayer2.spotify',
     objectPath: '/org/mpris/MediaPlayer2',
-    // interface: 'org.mpris.MediaPlayer2.Player'
-    interface: 'org.freedesktop.DBus.Properties'
+    interfaces: [
+        'org.mpris.MediaPlayer2.Player',
+        'org.freedesktop.DBus.Properties'
+    ]
 };
+let player = null;
 
-bus.getInterface(spotify.service, spotify.objectPath, spotify.interface, (err, iface) => {
+bus.getInterface(spotify.service, spotify.objectPath, spotify.interfaces[0], (err, iface) => {
+
+    if (err) {
+
+        console.log(err);
+        process.exit();
+    } else {
+        player = iface;
+        setTimeout(() => { player.Next(); }, 3000);
+    }
+});
+
+bus.getInterface(spotify.service, spotify.objectPath, spotify.interfaces[1], (err, iface) => {
 
     if (err) {
 
@@ -22,6 +37,8 @@ bus.getInterface(spotify.service, spotify.objectPath, spotify.interface, (err, i
         let lastState = null;
         iface.on('PropertiesChanged', (str, obj, arr) => {
 
+            // TODO: Find an interface/event that triggers once
+            // to avoid repeated states
             let newState = {
                 artist: obj.Metadata['xesam:artist'][0],
                 music: obj.Metadata['xesam:title'],
@@ -32,6 +49,7 @@ bus.getInterface(spotify.service, spotify.objectPath, spotify.interface, (err, i
             if (!_.isEqual(lastState, newState)) {
                 console.log(newState);
                 lastState = newState;
+                setTimeout(() => { player.Next(); }, 3000);
             }
         });
     }
