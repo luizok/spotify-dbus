bd4fe1eb378fcbaaff629d68c23703e5dad9422f
Luiz Felipe <luizfelipesmaciel@gmail.com>
2020-01-28 03:59:34 -0300

    Modularized main script

--- SpotifyManager.js
+++ SpotifyManager.js
@@ -12,46 +12,62 @@ let spotify = {
         'org.freedesktop.DBus.Properties'
     ]
 };
-let player = null;
+let player = {
+    nextTrack: null,
+    prevTrack: null,
+    playPause: null,
+    stopTrack: null,
+    currentTrack: null
+}
 
-bus.getInterface(spotify.service, spotify.objectPath, spotify.interfaces[0], (err, iface) => {
+let ifaceCallbackWrapper = (iface, method, callback) => {
 
-    if (err) {
+    iface[method]((err) => {
+        callback(err, player.currentTrack);
+    });
+};
 
-        console.log(err);
-        process.exit();
-    } else {
-        player = iface;
-        setTimeout(() => { player.Next(); }, 3000);
-    }
-});
+// TODO: Find elegant way to solve this callback hell
+// TODO: player.currentTrack actually shows to before-last one
+let buildManager = (callback) => {
 
-bus.getInterface(spotify.service, spotify.objectPath, spotify.interfaces[1], (err, iface) => {
+    bus.getInterface(spotify.service, spotify.objectPath, spotify.interfaces[0], (err, iface) => {
 
-    if (err) {
+        if (err)
+            callback(err, null);
+        else {
 
-        console.log(err);
-        process.exit();
-    } else {
+            player.nextTrack = (_callback) => { ifaceCallbackWrapper(iface, 'Next', _callback) };
+            player.prevTrack = (_callback) => { ifaceCallbackWrapper(iface, 'Previous', _callback) };
+            player.playPause = (_callback) => { ifaceCallbackWrapper(iface, 'PlayPause', _callback) };
+            player.stopTrack = (_callback) => { ifaceCallbackWrapper(iface, 'Stop', _callback) };
 
-        let lastState = null;
-        iface.on('PropertiesChanged', (str, obj, arr) => {
+            bus.getInterface(spotify.service, spotify.objectPath, spotify.interfaces[1], (err, _iface) => {
 
-            // TODO: Find an interface/event that triggers once
-            // to avoid repeated states
-            let newState = {
-                artist: obj.Metadata['xesam:artist'][0],
-                music: obj.Metadata['xesam:title'],
-                album: obj.Metadata['xesam:album'],
-                is_playing: obj.PlaybackStatus === 'Playing'
-            };
+                if (err) {
+                    callback(err, null);
+                } else {
 
-            if (!_.isEqual(lastState, newState)) {
-                console.log(newState);
-                lastState = newState;
-                setTimeout(() => { player.Next(); }, 3000);
-            }
-        });
-    }
+                    _iface.on('PropertiesChanged', (str, obj, arr) => {
 
-});
+                        let newState = {
+                            artist: obj.Metadata['xesam:artist'][0],
+                            music: obj.Metadata['xesam:title'],
+                            album: obj.Metadata['xesam:album'],
+                            isPlaying: obj.PlaybackStatus === 'Playing'
+                        };
+
+                        if (!_.isEqual(player.currentTrack, newState))
+                            player.currentTrack = newState;
+                    });
+
+                    callback(null, player);
+                }
+            });
+        }
+    });
+};
+
+module.exports = {
+    buildManager
+}
